3d7a6674521ae3ef47586221e0d575d4789167f5
Zana H <zhertica@gmail.com>
2020-05-21 21:33:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto=>{
+programmer_hash[{:yukihiro_matsumoto=>{
   :known_for =>"Ruby"
   :languages =>["LISP","C"]
-}]
+}}]
 programmer_hash
 end
 
